24f33a2dcbf41bd9b8333d7ae8ea75b0b0809ece
maikool <maikool22@me.com>
2023-05-12 20:51:49 -0300

    lenvanto infra en aws hasta el practico 5

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "terraform-state-mrodriguez"
-    dynamodb_table = "terraform-lock-table"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
+#terraform {
+ # backend "s3" {
+  #  encrypt        = true
+   # bucket         = "terraform-state-mrodriguez"
+    #dynamodb_table = "terraform-lock-table"
+    #key            = "terraform.tfstate"
+    #region         = "us-east-1"
+ # }
+#}
 # Posteriormente a crear el backend se crea el bucket s3 con el mismo nombre que el declarado y la tabla dynamo 
 # Luego de configurado el backend debo hacer un terraform init
