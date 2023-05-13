37e894873bfa16a71d27ef0638f98254fd8744ea
maikool <maikool22@me.com>
2023-05-12 21:18:29 -0300

    dejo comentado el ejercicio del backend

--- backend.tf
+++ backend.tf
@@ -7,5 +7,5 @@
     #region         = "us-east-1"
  # }
 #}
-# Posteriormente a crear el backend se crea el bucket s3 con el mismo nombre que el declarado y la tabla dynamo 
+# Posteriormente a crear el backend se crea el bucket s3 con el mismo nombre que el declarado y la tabla dynamo con el nombre LockID
 # Luego de configurado el backend debo hacer un terraform init
